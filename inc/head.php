7e832ed4d27b4a2c72f6703b705d840e3ccf2c9a
Erik de Vos <erik@pixelplus.nl>
2021-02-14 22:27:12 +0100

    Add basic project structure, styling and content.

---
+++ inc/head.php
@@ -0,0 +1,13 @@
+  <meta charset="utf-8">
+
+  <title>StrapFox. Simple scss + html5 framework with basic styling.</title>
+  <meta name="description" content="StrapFox. Simple scss + html5 framework with basic styling.">
+  <meta name="author" content="Erik de Vos">
+
+	<!-- Google fonts -->
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Ovo&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Quattrocento+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+	<!-- Custom styling -->
+  <link rel="stylesheet" href="/style/style.css?v=<?php date('Y-m-d H:i:s'); ?>">
---
+++ templates/homepage.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<?php include('inc/head.php'); ?>
+</head>
+
+<body>
+
+	<div class="page-wrapper">
+		<!-- Homepage header -->
+		<section class="header-wrapper">
+			&nbsp;
+		</section>
+
+		<!-- Homepage content -->
+		<section class="page-row-wrapper">
+			<div class="content-wrapper row">
+
+			<main class="flex column">
+
+				<h1>StrapFox</h1>
+				<h2>Simple SCSS + HTML5 framework with basic styling.</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam consequat viverra libero. Pellentesque habitant morbi
+				tristique senectus et netus et malesuada fames ac turpis egestas. Aenean vestibulum quis felis at sagittis.
+				<br> 
+				Maecenas faucibus convallis massa. Vivamus egestas ante a velit consectetur, eu pulvinar mauris porta. Class aptent taciti
+				sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus in facilisis ante, eu efficitur ex.</p>
+				</main>
+
+				<aside class="flex column">
+					Sidebar
+				</aside>
+
+			</div>
+		</section>
+
+	</div>
+
+	<script src="/scripts/scripts.js"></script>
+</body>
+
+</html>
